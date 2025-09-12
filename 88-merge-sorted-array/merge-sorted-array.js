e4bd8d33556c49184f58367f2fe054892ad983fe
Prateek K B <prateekbyadgi225@gmail.com>
2025-09-12 10:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (49.21%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -1,10 +1,4 @@
-/**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
+
 var merge = function(nums1, m, nums2, n) {
     let p1 = m - 1;
     let p2 = n - 1;
